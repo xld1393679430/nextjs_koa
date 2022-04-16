74c2a54c8be0c2b63fd68ab1cff368788db230c4
ld_xu <ld_xu@trip.com>
2022-04-17 01:00:13 +0800

    使用Link跳转 & 使用Router方式跳转

---
+++ pages/detail.js
@@ -0,0 +1,12 @@
+import { Button } from 'antd'
+class Index extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>Detail</p>
+            </div>
+        )
+    }
+}
+
+export default Index
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
-import { Button } from 'antd'
-class Index extends React.Component{
-    render(){
-        return (
-            <div>
-                <title>Index</title>
-                <Button>按钮</Button>
-            </div>
-        )
-    }
+import { Button } from "antd";
+import Link from "next/link";
+import Router from "next/router";
+class Index extends React.Component {
+  handleRouterNavigation = () => {
+    Router.push("/detail");
+  };
+  
+  render() {
+    return (
+      <div>
+        <p>Index</p>
+        <Link href="/detail" titale="aaaa">
+          <Button>使用Link跳转到Detail页面</Button>
+        </Link>
+
+        <br />
+        <Button onClick={this.handleRouterNavigation}>
+          使用Router方式跳转
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default Index
+export default Index;

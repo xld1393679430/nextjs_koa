702bfbd38b85b5e0c07c5448c30eb64bf203d5a8
ld_xu <ld_xu@trip.com>
2022-04-17 19:53:19 +0800

    配置Redis 测试添加session和删除session

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    gitlab: {
+        client_id: 'f63b0a7e349f8dbd0e93',
+        client_secret: '451eb4c52627ebc6f298fd6c1654119a77c2d3b1'
+    },
+    url: 'https://github.com/login/oauth/authorize?client_id=f63b0a7e349f8dbd0e93&scope=user,repo',
+    code: '5f40f0d298bbee638118'
+}
--- server.js
+++ server.js
@@ -1,27 +1,53 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 const next = require("next");
+const session = require("koa-session");
+const Redis = require("ioredis");
+const RedisSessionStore = require("./server/session-store");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const redis = new Redis();
 
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
 
+  server.keys = ["xld develop GitHub App"];
+  const SESSION_CONFIG = {
+    key: "jid",
+    store: new RedisSessionStore(redis),
+  };
+
+  server.use(session(SESSION_CONFIG, server));
 
   // 解决路由映射刷新页面404的问题
-  router.get('/next-demo/detail/:id', async (ctx) => {
+  router.get("/next-demo/detail/:id", async (ctx) => {
     const id = ctx.params.id;
     await handle(ctx.req, ctx.res, {
-      pathname: '/next-demo/detail',
+      pathname: "/next-demo/detail",
       query: {
-        id
-      }
-    })
+        id,
+      },
+    });
     ctx.response = false;
-  })
+  });
+
+  // 增加session
+  router.get("/set/user", async (ctx) => {
+    ctx.session.user = {
+      name: "xld",
+      age: 22,
+    };
+    ctx.body = "set session success";
+  });
+
+  // 删除session
+  router.get("/delete/user", async (ctx) => {
+    ctx.session = null;
+    ctx.body = "delete session success";
+  });
 
   server.use(router.routes());
 

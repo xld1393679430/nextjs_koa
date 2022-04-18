eba0b5592bdc3464315bc89789da11d5c096ea17
ld_xu <ld_xu@trip.com>
2022-04-18 14:26:52 +0800

    通过监听Router.events事件增加全局loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import App, { Container } from "next/app";
 import { Provider } from "react-redux";
+import Router from "next/router";
 import "antd/dist/antd.css";
 import withRedux from "../lib/with-redux";
 import Layout from "../components/Layout";
+import Loading from "../components/Loading";
 
 class MyApp extends App {
   static async getInitialProps(ctx) {
@@ -18,11 +20,41 @@ class MyApp extends App {
     };
   }
 
+  state = {
+    loading: false,
+  };
+
+  startLoading = () => {
+    this.setState({
+      loading: true,
+    });
+  };
+
+  stopLoading = () => {
+    this.setState({
+      loading: false,
+    });
+  };
+
+  componentDidMount() {
+    Router.events.on("routeChangeStart", this.startLoading);
+    Router.events.on("routeChangeComplete", this.stopLoading);
+    Router.events.on("routeChangeError", this.stopLoading);
+  }
+
+  componentWillUnmount() {
+    Router.events.off("routeChangeStart", this.startLoading);
+    Router.events.off("routeChangeComplete", this.stopLoading);
+    Router.events.off("routeChangeError", this.stopLoading);
+  }
+
   render() {
     const { Component, pageProps, reduxStore } = this.props;
+    const { loading } = this.state;
     return (
       <Container>
         <Provider store={reduxStore}>
+          {loading && <Loading />}
           <Layout>
             <Component {...pageProps} />
           </Layout>
--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,1 +1,11 @@
-export default () => <div>Detail Page</div>
+const Index = () => <div>Detail Page</div>;
+
+Index.getInitialProps = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({});
+    }, 1500);
+  });
+};
+
+export default Index;
